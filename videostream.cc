2713bdeaba0d3a76f88df9973db0e129c7573dea
Roshnee <roshneekishore@yahoo.in>
2017-11-21 16:52:56 +0100

    Split image

--- videostream.cc
+++ videostream.cc
@@ -4,6 +4,7 @@
 using namespace std;
 
 int main(int, char**) {
+
     cv::VideoCapture vcap;
     cv::Mat image;
 
@@ -11,7 +12,7 @@ int main(int, char**) {
     /* it may be an address of an mjpeg stream, 
     e.g. "http://user:pass@cam_address:8081/cgi/mjpg/mjpg.cgi?.mjpg" */
 
-    //open the video stream and make sure it's opened
+    //check if the videostream is open
     if(!vcap.open(videoStreamAddress)) {
         std::cout << "Error opening video stream or file" << std::endl;
         return -1;
@@ -21,14 +22,22 @@ int main(int, char**) {
     //It's important to create this window outside of the `for` loop
     //Otherwise this window will be created automatically each time you call
     //`imshow(...)`, which is very inefficient. 
+
     cv::namedWindow("Output Window");
+    cv::namedWindow("Left");
+    cv::namedWindow("Right");
     cout<<"cap is opened"<<endl;
     for(;;) {
         if(!vcap.read(image)) {
             std::cout << "No frame" << std::endl;
             cv::waitKey();
         }
+        //Mat croppedFrame = frame(Rect(0, frame.rows/2, frame.cols, frame.rows/2));
+        cv::Mat imgL = image(cv::Range(0, image.rows), cv::Range(0, image.cols / 2));
+        cv::Mat imgR = image(cv::Range(0, image.rows), cv::Range(image.cols / 2 + 1, image.cols));
         cv::imshow("Output Window", image);
+        cv::imshow("Left",imgL);
+        cv::imshow("Right",imgR);
         if(cv::waitKey(1) >= 0) break;
     }
 }
