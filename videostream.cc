09905e6528bd53ce5b4a3f2324c5b2335a7e9286
Roshnee <roshneekishore@yahoo.in>
2017-11-21 16:13:11 +0100

    Input uv4l videostream into opencv c++

---
+++ videostream.cc
@@ -0,0 +1,34 @@
+nclude "opencv2/opencv.hpp"
+#include "opencv2/highgui.hpp"
+#include <iostream>
+using namespace std;
+
+int main(int, char**) {
+    cv::VideoCapture vcap;
+    cv::Mat image;
+
+    const std::string videoStreamAddress = "http://10.201.131.67:9090/stream/video.mjpeg";
+    /* it may be an address of an mjpeg stream, 
+    e.g. "http://user:pass@cam_address:8081/cgi/mjpg/mjpg.cgi?.mjpg" */
+
+    //open the video stream and make sure it's opened
+    if(!vcap.open(videoStreamAddress)) {
+        std::cout << "Error opening video stream or file" << std::endl;
+        return -1;
+    }
+
+    //Create output window for displaying frames. 
+    //It's important to create this window outside of the `for` loop
+    //Otherwise this window will be created automatically each time you call
+    //`imshow(...)`, which is very inefficient. 
+    cv::namedWindow("Output Window");
+    cout<<"cap is opened"<<endl;
+    for(;;) {
+        if(!vcap.read(image)) {
+            std::cout << "No frame" << std::endl;
+            cv::waitKey();
+        }
+        cv::imshow("Output Window", image);
+        if(cv::waitKey(1) >= 0) break;
+    }
+}
